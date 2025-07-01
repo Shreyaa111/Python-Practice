7f2ab5d22a3d535495d0f05bba793e8a660622df
Shreyaa111 <34883411+Shreyaa111@users.noreply.github.com>
2025-07-01 21:27:24 +0530

    Add files via upload
    Added first practice script

---
+++ Practice_codes.py
@@ -0,0 +1,12 @@
+# print("Hi"+" "+"Shreya" + "\nGood Night")
+# print("Hi "+"Shreya" + "\nGood Night")
+# print("A")
+# a, b, c= 1, 2 ,3
+# print(a, b, c)
+# input('had your lunch?')
+# print (("hi shreya ") + (input ("had your lunch?")))
+
+# human = "hello "
+# print(human)
+# print ((human) + input("what is name" ), (len(human)))
+# print(len(input("what is name" )))

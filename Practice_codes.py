6a81a7be2e548d6cb26b997a195e5aadc4cf06e3
Shreyaa111 <34883411+Shreyaa111@users.noreply.github.com>
2025-07-03 13:41:22 +0530

    Update Practice_codes.py

--- Practice_codes.py
+++ Practice_codes.py
@@ -1,24 +1,34 @@
+#Print string with concatenation with space
 print("Hi"+" "+"Shreya" + "\nGood Night")
 
+#Print string with concatenation without space
 print("Hi "+"Shreya" + "\nGood Night")
 
+#print
 print("A")
 
+#store in variable
 a, b, c= 1, 2 ,3
 print(a, b, c)
 
+#input function
 input('had your lunch?')
 print (("hi shreya ") + (input ("had your lunch?")))
 
+#length of name
 human = "hello "
 print(human)
 print ((human) + input("what is name" ), (len(human)))
 print(len(input("what is name" )))
 
-
+#length of character with storing in variable
 name = "shreya"
 length=len(name)
 print(length)
+
+# We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice.
+# Write 3 lines of code to switch the contents of the variables. You are not allowed 
+# to type the words "milk" or "juice". You are only allowed to use variables to solve this exercise.
 
 glass1 = "milk"
 glass2 = "juice"

548aa654665d5e1362bd271aeb3b45dc1e68ecd8
Shreyaa111 <34883411+Shreyaa111@users.noreply.github.com>
2025-07-03 15:10:32 +0530

    Create length.py

---
+++ length.py
@@ -0,0 +1,10 @@
+#length of name
+human = "hello "
+print(human)
+print ((human) + input("what is name" ), (len(human)))
+print(len(input("what is name" )))
+
+#length of character with storing in variable
+name = "shreya"
+length=len(name)
+print(length)

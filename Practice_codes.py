64cee5d5c7f8bfeb7620b04d19b6032e39398239
Shreyaa111 <34883411+Shreyaa111@users.noreply.github.com>
2025-07-03 13:36:41 +0530

    Update Practice_codes.py

--- Practice_codes.py
+++ Practice_codes.py
@@ -1,12 +1,46 @@
-# print("Hi"+" "+"Shreya" + "\nGood Night")
-# print("Hi "+"Shreya" + "\nGood Night")
-# print("A")
-# a, b, c= 1, 2 ,3
-# print(a, b, c)
-# input('had your lunch?')
-# print (("hi shreya ") + (input ("had your lunch?")))
+print("Hi"+" "+"Shreya" + "\nGood Night")
 
-# human = "hello "
-# print(human)
-# print ((human) + input("what is name" ), (len(human)))
-# print(len(input("what is name" )))
+print("Hi "+"Shreya" + "\nGood Night")
+
+print("A")
+
+a, b, c= 1, 2 ,3
+print(a, b, c)
+
+input('had your lunch?')
+print (("hi shreya ") + (input ("had your lunch?")))
+
+human = "hello "
+print(human)
+print ((human) + input("what is name" ), (len(human)))
+print(len(input("what is name" )))
+
+
+name = "shreya"
+length=len(name)
+print(length)
+
+glass1 = "milk"
+glass2 = "juice"
+temp = glass1
+glass1=glass2
+glass2 = temp
+print("glass1 =", glass1 , "and glass2 =", glass2)
+
+
+#band name generator project
+
+print("Welcome to the Band name generator\n")
+Place = input("Where are you from?\n")
+Pet = input("Your pet name\n")
+print("Place:",Place , "and Pet:", Pet)
+print("Your Band name is: "+ Place+" "+Pet) #this is best 
+
+# learnt below :
+# printing
+# string
+# input
+# variables
+  
+
+

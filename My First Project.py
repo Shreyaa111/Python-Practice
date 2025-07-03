0554de9e9c391a7edf77e316a3b05b8ca44ed627
Shreyaa111 <34883411+Shreyaa111@users.noreply.github.com>
2025-07-03 15:12:17 +0530

    Create My First Project.py

---
+++ My First Project.py
@@ -0,0 +1,7 @@
+#band name generator project
+
+print("Welcome to the Band name generator\n")
+Place = input("Where are you from?\n")
+Pet = input("Your pet name\n")
+print("Place:",Place , "and Pet:", Pet)
+print("Your Band name is: "+ Place+" "+Pet) #this is best 
